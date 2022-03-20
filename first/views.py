5cb920ec101fcbc734f9d46859542d2e2530cb6b
seoyeong kim <ksy98102@gmail.com>
2022-03-21 00:42:09 +0900

    Django의 기본 구조

--- first/views.py
+++ first/views.py
@@ -3,20 +3,40 @@ from django.http import HttpResponse
 from django.template import loader
 from datetime import datetime
 
+import random
+
 # Create your views here.
 def index(request):
     now = datetime.now()
     context = {
         'current_date':now
     }
-    return render(request, 'index.html', context)
+    return render(request, 'first/index.html', context)
 
 
 def select(request):
-    context={'num':4}
-    return render(request, 'select.html', context)
+    context = {}
+    return render(request, 'first/select.html', context)
 
 
 def result(request):
-    context = {}
-    return render(request, 'result.html', context)
+    chosen = int(request.GET['number'])
+
+    results = []
+    if chosen >= 1 and chosen <= 45:
+        results.append(chosen)
+
+    box=[]
+    for i in range(0,45):
+        if chosen != i+1:
+            box.append(i+1)
+
+    random.shuffle(box)
+
+    while len(results) < 6:
+        results.append(box.pop())
+
+    context = {
+        'numbers': results
+    }
+    return render(request, 'first/result.html', context)
